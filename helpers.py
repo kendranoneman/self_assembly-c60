9a245a72583e11b891eab19546a6c7e5767d3cb1
Kendra Noneman <37158560+kendranoneman@users.noreply.github.com>
2018-11-07 10:02:01 -0700

    Create helpers.py
    Example helpers.py (defines names of jobs)

---
+++ helpers.py
@@ -0,0 +1,30 @@
+import os
+import numpy
+
+def make_name(mix,q,e,T,N,cc_eps):
+    return "mix{}-q{}-e{}-T{}-N{}-ccep{}".format(mix,q,e,T,N,cc_eps)
+
+def checkdir(name):
+    if not os.path.exists(name):
+        os.makedirs(name)
+    return
+
+def get_params(i):
+    line = numpy.loadtxt('params.txt')[i]
+    print(line)
+    mix = int(line[1])
+    q = int(line[2])
+    e = int(line[3])
+    T = float(line[4])
+    N = int(line[5])
+    cc_eps = float(line[6])
+    return mix,q,e,T,N,cc_eps
+
+def make_filenames(jobname):
+    m="runs/"+jobname+"/msd.log"
+    i="runs/"+jobname+"/init.xml"
+    t="runs/"+jobname+"/traj.dcd"
+    e="runs/"+jobname+"/restart.xml"
+    l="runs/"+jobname+"/log.log"
+    checkdir("runs/"+jobname)
+    return m,i,t,e,l
